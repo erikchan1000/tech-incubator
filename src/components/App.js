653e416457924fc67f8d525b60c02b1e6c973b7a
Erik Chan <chanerik1000@gmail.com>
2022-07-12 20:40:34 -0700

    Centered!

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Signup from "./Signup"
-import {Container} from 'react-bootstrap'
+import Signup from "./Signup";
+import { Container } from 'react-bootstrap';
+import "./App.css";
 
 function App() {
   return (
     <Container className="d-flex align-items-center"
     style={{ minHeight: "100vh"}}>
-      <div className="w-100" style={{maxWidth: '400px'}}>
+      <div className="w-100" style={{maxWidth: '30rem', maxHeight: ''}}>
       <Signup/>
       </div>
     </Container>
