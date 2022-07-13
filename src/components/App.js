f84e1463b258c9fc4c79406aedff6ebd990ef9cd
Erik Chan <chanerik1000@gmail.com>
2022-07-13 00:47:31 -0700

    finished AuthContext

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Signup from "./Signup";
 import { Container } from 'react-bootstrap';
 import "./App.css";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
+    <AuthProvider>
     <Container className="d-flex align-items-center"
     style={{ minHeight: "100vh"}}>
       <div className="w-100" style={{maxWidth: '30rem', maxHeight: ''}}>
       <Signup/>
       </div>
     </Container>
+    </AuthProvider>
   )
 }
 
